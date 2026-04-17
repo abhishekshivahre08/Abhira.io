e9491b699da5f5a8bf883ce7be5c5cf3e17447da
Abhishek Shivhare <abhishekshivahre08@gmail.com>
2026-04-17 14:12:53 +0530

    adding a server url

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/homePage';
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://abhira-io.onrender.com";
 import useGetCurrentUser from './hooks/useGetCurrentUser';
 import { useSelector } from 'react-redux';
 import Dashboard from './pages/home/Dashboard';
